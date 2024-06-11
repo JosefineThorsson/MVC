0fe7a7dadcce010d92da18d7a93520a09adda399
Josefine Thorsson <jotn23@student.bth.se>
2024-06-11 19:16:09 +0200

    Fixing access

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/style.min.css';
+import './styles/app.css';
 import hello from './js/hello.js';
 
 console.log(hello());
